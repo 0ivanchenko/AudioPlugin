c7f5170dc893760f58e7713051a3d5166dfdeebd
ivanchenko <vadim.iv159@mail.ru>
2024-10-01 02:55:33 +0700

    Создано тело проекта. Добавлены две структуры: аудио буффер и аудио эффект.

--- AudioPlugin/AudioPlugin.cpp
+++ AudioPlugin/AudioPlugin.cpp
@@ -1,1 +1,22 @@
-﻿
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+typedef struct {
+	float* data; // указатель на массив данных аудиосигнала
+	size_t size; // размер буфера (количество сэмплов)
+	int sampleRate; // частота дискретизации
+} AudioBuffer;
+
+typedef struct {
+	float mix; // коэффициент смешивания сухого и обработанного сигнала
+	void (*process)(AudioBuffer* input, AudioBuffer* output); // функция для обработки входного аудиобуфера
+} AudioEffect;
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+
+}
